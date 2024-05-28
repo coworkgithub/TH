8528e34c9b5b314029eb4d24db723e0d663efc98
taehoon0518 <cjsxogns0518@gmail.com>
2024-05-28 13:51:09 +0900

    Changes to be committed: 	modified:   board-back/src/main/java/com/sugang/boardback/controller/UserController.java 	new file:   board-back/src/main/java/com/sugang/boardback/dto/request/user/PatchNicknameRequestDto.java 	new file:   board-back/src/main/java/com/sugang/boardback/dto/request/user/PatchProfileImageRequestDto.java 	new file:   board-back/src/main/java/com/sugang/boardback/dto/response/user/PatchNicknameResponseDto.java 	new file:   board-back/src/main/java/com/sugang/boardback/dto/response/user/PatchProfileImageResponseDto.java 	modified:   board-back/src/main/java/com/sugang/boardback/entity/UserEntity.java 	modified:   board-back/src/main/java/com/sugang/boardback/service/UserService.java 	modified:   board-back/src/main/java/com/sugang/boardback/service/implement/UserServiceImplement.java

--- board-back/src/main/java/com/sugang/boardback/controller/UserController.java
+++ board-back/src/main/java/com/sugang/boardback/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.sugang.boardback.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sugang.boardback.dto.request.user.PatchNicknameRequestDto;
+import com.sugang.boardback.dto.request.user.PatchProfileImageRequestDto;
 import com.sugang.boardback.dto.response.user.GetSignInUserResponseDto;
 import com.sugang.boardback.dto.response.user.GetUserResponseDto;
+import com.sugang.boardback.dto.response.user.PatchNicknameResponseDto;
+import com.sugang.boardback.dto.response.user.PatchProfileImageResponseDto;
 import com.sugang.boardback.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,6 +39,24 @@ public class UserController {
         @AuthenticationPrincipal String email ) {
             ResponseEntity<? super GetSignInUserResponseDto> response = userService.getSignInUser(email);
             return response;
-        }
+    }
     
+    @PatchMapping("/nickname")
+    public ResponseEntity<? super PatchNicknameResponseDto> patchNickname(
+        @RequestBody @Valid PatchNicknameRequestDto requestBody,
+        @AuthenticationPrincipal String email
+    ){
+        ResponseEntity<? super PatchNicknameResponseDto> response = userService.patchNickname(requestBody, email);
+        return response;
+    }
+
+    @PatchMapping("/profile-image")
+    public ResponseEntity<? super PatchProfileImageResponseDto> patchProfileImage(
+        @RequestBody @Valid PatchProfileImageRequestDto requestBody,
+        @AuthenticationPrincipal String email
+    ){
+        ResponseEntity<? super PatchProfileImageResponseDto> response = userService.patchProfileImage(requestBody, email);
+        return response; 
+
+    }
 }
